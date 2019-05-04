a77a0e9c4de0154d8e3ace150e7876312ae33484
Omar <omarshantirn@gmail.com>
2019-05-04 17:33:22 -0500

    Updated Dropdown

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -11,8 +11,13 @@ export class SearchBarComponent implements OnInit {
   public sliderType = SliderType;
   public priceRange: PriceRange = new PriceRange(0, 24);
   
+  public genders: String[] = ["Male", "Female", "Other", "Prefer not to specify"];
+  public languages: String[] = ["English", "Español", "Francais", "Polish", "عربي", "كردي", "Mandarin"];
+
   public showFilters = false;
 
+  public checked: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
